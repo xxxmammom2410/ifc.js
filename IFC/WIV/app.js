32788a4da86ca9c4d58e96226c351c2c9c751e18
xxxmammom2410 <akira2410.luminous@gmail.com>
2022-10-18 22:53:52 +0900

    WIV

---
+++ IFC/WIV/app.js
@@ -0,0 +1,20 @@
+import { Color } from 'three';
+import { IfcViewerAPI } from 'web-ifc-viewer';
+
+const container = document.getElementById('viewer-container');
+const viewer = new IfcViewerAPI({ container, backgroundColor: new Color(0xffffff) });
+
+// Create grid and axes
+viewer.grid.setGrid();
+viewer.axes.setAxes();
+
+async function loadIfc(url) {
+		// Load the model
+    const model = await viewer.IFC.loadIfcUrl(url);
+
+		// Add dropped shadow and post-processing efect
+    await viewer.shadowDropper.renderShadow(model.modelID);
+    viewer.context.renderer.postProduction.active = true;
+}
+
+loadIfc('../../models/_ifc/04.ifc');
