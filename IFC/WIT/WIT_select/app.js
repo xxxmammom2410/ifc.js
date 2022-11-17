53090bb4f0002aa1cbe2a669a8cc49d648089976
ak-nishimoto <akira2410.luminous@gmail.com>
2022-11-18 08:24:09 +0900

    WIT_raycast

--- IFC/WIT/WIT_select/app.js
+++ IFC/WIT/WIT_select/app.js
@@ -3,6 +3,7 @@ import {
   AxesHelper,
   DirectionalLight,
   GridHelper,
+  MeshLambertMaterial,
   PerspectiveCamera,
   Scene,
   WebGLRenderer,
@@ -16,21 +17,16 @@ import { acceleratedRaycast,computeBoundsTree,disposeBoundsTree } from "three-me
 
 const input = document.getElementById("file-input");
 
-
+//  1 Scene 
 //Creates the Three.js scene
 const scene = new Scene();
-
+const threeCanvas = document.getElementById("three-canvas");
 //Object to store the size of the viewport
 const size = {
   width: window.innerWidth,
   height: window.innerHeight,
 };
-
-//Creates the camera (point of view of the user)
-const camera = new PerspectiveCamera(75, size.width / size.height);
-camera.position.z = 15;
-camera.position.y = 13;
-camera.position.x = 8;
+// 2 Object
 
 //Creates the lights of the scene
 const lightColor = 0xffffff;
@@ -42,12 +38,82 @@ const directionalLight = new DirectionalLight(lightColor, 2);
 directionalLight.position.set(0, 10, 0);
 scene.add(directionalLight);
 
-//Sets up the renderer, fetching the canvas of the HTML
-const threeCanvas = document.getElementById("three-canvas");
-const renderer = new WebGLRenderer({ canvas: threeCanvas, alpha: true });
-renderer.setSize(size.width, size.height);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
+// Load IFC Model
+const ifcLoader = new IFCLoader();
+// Sets up optimized picking
+ifcLoader.ifcManager.setupThreeMeshBVH(computeBoundsTree,disposeBoundsTree,acceleratedRaycast);
+
+const ifcModels = [];
+
+input.addEventListener(
+    "change",
+    async (changed) => {
+        const ifcURL = URL.createObjectURL(changed.target.files[0]);
+        const model = await ifcLoader.loadAsync(ifcURL);
+        scene.add(model);
+        console.log(model);
+        // threeのraycast用のarrayに追加
+        ifcModels.push(model);
+    },
+    false
+);
+
+// Creates subset material
+const preselectMat = new MeshLambertMaterial({
+  transparent: true,
+  opacity: 0.6,
+  color: 0xff88ff,
+  depthTest: false
+})
+
+const selectMat = new MeshLambertMaterial({
+  transparent: true,
+  opacity: 0.6,
+  color: 0xff00ff,
+  depthTest: false })
+
+
+// highlight effect
+// Reference  to the previous selection
+let preselectModel = {id: -1};
+
+function highlight(event, material, model){
+  const found = cast(event)[0];
+  if(found){
+
+    // Gets model ID
+    model.id = found.object.modelID;
+    console.log(found);
+
+    // Gets Express ID
+    const index = found.faceIndex;
+    const geometry = found.object.geometry;
+    const id = ifcLoader.ifcManager.getExpressId(geometry,index);
+
+    // Creates subset
+    ifcLoader.ifcManager.createSubset({
+      modelID: model.id,
+      ids: [id],
+      material:material,
+      scene: scene,
+      removePrevious: true
+    })
+  } else {
+    // Removes previous highlight
+    ifcLoader.ifcManager.removeSubset(model.id, material);
+  }
+}
+
+window.onmousemove = (event) => highlight(event,preselectMat,preselectModel)
+
+const selectModel = { id: - 1};
+window.ondblclick = (event) => highlight(
+      event,
+      selectMat,
+      selectModel );
+
+// Helper Object
 //Creates grids and axes in the scene
 const grid = new GridHelper(50, 30);
 scene.add(grid);
@@ -57,10 +123,6 @@ axes.material.depthTest = false;
 axes.renderOrder = 1;
 scene.add(axes);
 
-//Creates the orbit controls (to navigate the scene)
-const controls = new OrbitControls(camera, threeCanvas);
-controls.enableDamping = true;
-controls.target.set(-2, 0, 0);
 
 //raycast
 const raycaster = new Raycaster();
@@ -102,14 +164,24 @@ function pick(event){
 // threeのcanvas上をダブルクリックでレイキャスト発射
 threeCanvas.ondblclick = (event) => pick(event);
 
-//Animation loop
-const animate = () => {
-  controls.update();
-  renderer.render(scene, camera);
-  requestAnimationFrame(animate);
-};
 
-animate();
+// 3　Camera
+//Creates the camera (point of view of the user)
+const camera = new PerspectiveCamera(75, size.width / size.height);
+camera.position.z = 15;
+camera.position.y = 13;
+camera.position.x = 8;
+
+
+// 4 Renderer
+
+
+//Sets up the renderer, fetching the canvas of the HTML
+
+const renderer = new WebGLRenderer({ canvas: threeCanvas, alpha: true });
+renderer.setSize(size.width, size.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
 
 //Adjust the viewport to the size of the browser
 window.addEventListener("resize", () => {
@@ -119,21 +191,20 @@ window.addEventListener("resize", () => {
   renderer.setSize(size.width, size.height);
 });
 
-const ifcLoader = new IFCLoader();
-// Sets up optimized picking
-ifcLoader.ifcManager.setupThreeMeshBVH(computeBoundsTree,disposeBoundsTree,acceleratedRaycast);
 
-const ifcModels = [];
+// 5 Animation
+//Creates the orbit controls (to navigate the scene)
+const controls = new OrbitControls(camera, threeCanvas);
+controls.enableDamping = true;
+controls.target.set(-2, 0, 0);
 
-input.addEventListener(
-    "change",
-    async (changed) => {
-        const ifcURL = URL.createObjectURL(changed.target.files[0]);
-        const model = await ifcLoader.loadAsync(ifcURL);
-        scene.add(model);
-        console.log(model);
-        // threeのraycast用のarrayに追加
-        ifcModels.push(model);
-    },
-    false
-);
+//Animation loop
+const animate = () => {
+  controls.update();
+  renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+};
+
+animate();
+
+
