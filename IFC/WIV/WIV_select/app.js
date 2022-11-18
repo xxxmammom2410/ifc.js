b6dbfac8714f6fc64a20ff0ec8f90352a24b5493
xxxmammom2410 <akira2410.luminous@gmail.com>
2022-11-18 11:29:57 +0900

    WIV_select

---
+++ IFC/WIV/WIV_select/app.js
@@ -0,0 +1,23 @@
+import { Color } from 'three';
+import { IfcViewerAPI } from 'web-ifc-viewer';
+
+const container = document.getElementById('viewer-container');
+const viewer = new IfcViewerAPI({ container, backgroundColor: new Color(0xffffff) });
+
+// Create grid and axes
+viewer.grid.setGrid();
+viewer.axes.setAxes();
+
+async function loadIfc(url) {
+		// Load the model
+    const model = await viewer.IFC.loadIfcUrl(url);
+
+		// Add dropped shadow and post-processing efect
+    await viewer.shadowDropper.renderShadow(model.modelID);
+    viewer.context.renderer.postProduction.active = true;
+}
+
+loadIfc('../../../models/_ifc/04.ifc');
+
+window.ondblclick = async () => await viewer.IFC.selector.pickIfcItem();
+window.onmousemove = async () => await viewer.IFC.selector.prePickIfcItem();
