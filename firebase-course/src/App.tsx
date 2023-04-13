ddcc1b30a125fc2b72d106abe15a686eb13f31c5
xxxmammom2410 <akira2410.luminous@gmail.com>
2023-04-14 08:29:46 +0900

    firebase-course_GoogleAuthentication

--- firebase-course/src/App.tsx
+++ firebase-course/src/App.tsx
@@ -1,11 +1,12 @@
 import { FC } from "react";
 import { getApp } from "firebase/app";
 import "./App.css";
+import { Main } from "./components/main";
 
 export const App: FC = () => {
   return (
     <div className="App">
-      {JSON.stringify(getApp())}
+      <Main />
     </div>
   );
 };
---
+++ firebase-course/src/components/auth/google-auth.tsx
@@ -0,0 +1,17 @@
+import { FC } from "react";
+import { getAuth, signInWithPopup,GoogleAuthProvider } from 'firebase/auth';
+export const GoogleAuth: FC = () => {
+
+  const auth = getAuth();
+  const provider = new GoogleAuthProvider();
+ 
+  const onLoginClick = () => {
+    console.log("Google Login");
+    // promiseでポップアップ処理
+    signInWithPopup(auth, provider);
+  };
+
+
+  return <button className="coolButton"  onClick={onLoginClick}>Google Login</button>;
+};
+
